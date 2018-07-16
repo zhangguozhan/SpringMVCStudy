e01d73b55d5d2eae58e6d7ce3b0cba166c3502ba
zhangguozhan <1299355661@qq.com>
2018-07-16 22:20:16 +0800

    Change-Id: I0000000000000000000000000000000000000000

---
+++ src/main/java/com/spring/conifg/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.spring.conifg;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private static final Logger logger = LogManager.getLogger(SecurityConfig.class);
+
+	public SecurityConfig() {
+		logger.info("==============SecurityConfig.class=======create=============");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin().loginPage("/login").and().logout().logoutSuccessUrl("/").and().rememberMe()
+				.tokenRepository(new InMemoryTokenRepositoryImpl()).tokenValiditySeconds(2419200).key("spittrKey").and()
+				.httpBasic().realmName("Spittr").and().authorizeRequests().antMatchers("/").authenticated()
+				.antMatchers("/spitter/me").authenticated().antMatchers(HttpMethod.POST, "/spittles").authenticated()
+				.anyRequest().permitAll();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		logger.info("==============SecurityConfig.configure=======create=============");
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and()
+		.withUser("admin").password("password").roles("USER", "ADMIN");
+	}
+
+}
